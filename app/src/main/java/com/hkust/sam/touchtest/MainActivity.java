7b87d9a43cf66658dc4fefd818fc925da71d4a63
Zhenyu WANG <wangzhenyu1260@126.com>
2015-11-15 21:02:21 +0800

    swip OK!

--- app/src/main/java/com/hkust/sam/touchtest/MainActivity.java
+++ app/src/main/java/com/hkust/sam/touchtest/MainActivity.java
@@ -1,9 +1,18 @@
 package com.hkust.sam.touchtest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
+
+import com.hkust.sam.ipc.RootShellCmd;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     RootShellCmd rootShellCmd = new RootShellCmd();
@@ -22,7 +31,31 @@ public class MainActivity extends AppCompatActivity {
     public void VolumeDown(View view){
         rootShellCmd.simulateKey(KeyEvent.KEYCODE_VOLUME_DOWN);
     }
-    public void VolumeMute(View view){
-        rootShellCmd.simulateKey(KeyEvent.KEYCODE_VOLUME_MUTE);
+
+    public void showInfo(View view){
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        int width = displaymetrics.widthPixels;
+        int colorDep = getWindowManager().getDefaultDisplay().getPixelFormat();
+//        String android_id = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String now = df.format(c.getTime());
+
+        String text = "手机分辨率是："+width+"*"+height+" 手机色深是："+colorDep+" IMEI是："+telephonyManager.getDeviceId()+" Time: "+now;
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    public void Home(View view){
+//        rootShellCmd.simulateKey(KeyEvent.KEYCODE_MOVE_HOME);
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int y = displaymetrics.heightPixels;
+        int x = displaymetrics.widthPixels;
+        String temp = x/2 + " 1 "+ x/2 +" "+y;
+        rootShellCmd.simulateSwip(temp);
     }
 }
