e18c21eea0c1f828895b0904a57a49c44dd14756
hoz.danny <hoz.danny@gmail.com>
2015-11-16 21:20:34 +0800

    Fix Bug: remove relative layout listener set listen to the btn in the code instead of in the layout file

--- app/src/main/java/com/hkust/sam/touchtest/MainActivity.java
+++ app/src/main/java/com/hkust/sam/touchtest/MainActivity.java
@@ -8,6 +8,7 @@ import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.hkust.sam.ipc.RootShellCmd;
@@ -15,12 +16,26 @@ import com.hkust.sam.ipc.RootShellCmd;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     RootShellCmd rootShellCmd = new RootShellCmd();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button up = (Button) findViewById(R.id.up_btn);
+        up.setOnClickListener(this);
+
+        Button down = (Button) findViewById(R.id.down_btn);
+        down.setOnClickListener(this);
+
+        Button info = (Button) findViewById(R.id.info_btn);
+        info.setOnClickListener(this);
+
+        Button home = (Button) findViewById(R.id.home_btn);
+        home.setOnClickListener(this);
+
+
 
     }
 
@@ -40,28 +55,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //when up btn clicked
-    public void VolumeUp(View view){
+    public void volumeUp(){
         Intent intent = new Intent(getApplicationContext(), TouchController.class);
         intent.setAction(Constant.ACTION_VOLUME_UP);
         startService(intent);
     }
 
     //when down btn clicked
-    public void VolumeDown(View view){
+    public void volumeDown(){
         Intent intent = new Intent(getApplicationContext(), TouchController.class);
         intent.setAction(Constant.ACTION_VOLUME_DOWN);
         startService(intent);
     }
 
     //when home btn clicked
-    public void Home(View view){
+    public void home(){
         Intent intent = new Intent(getApplicationContext(), TouchController.class);
         intent.setAction(Constant.ACTION_NOTIFICATION);
         startService(intent);
     }
 
     //when info btn clicked
-    public void showInfo(View view){
+    public void showInfo(){
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         int height = displaymetrics.heightPixels;
@@ -78,4 +93,22 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.up_btn:
+                volumeUp();
+                break;
+            case R.id.down_btn:
+                volumeDown();
+                break;
+            case R.id.info_btn:
+                showInfo();
+                break;
+            case R.id.home_btn:
+                home();
+                break;
+            default:
+        }
+    }
 }
